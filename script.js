87be763d298daf728631421c2d522b9d9b86d4ac
MehmetKagan <mehmetkagankokoglu@gmail.com>
2023-03-28 22:44:33 +0300

    docs: Add buttons to html, add css styles for buttons and slides, add "remove class" for grids to js.
    Add buttons to html, add css styles for buttons and slides, add "remove class" for grids to js.

--- script.js
+++ script.js
@@ -11,22 +11,10 @@ for (let i = 0; i < 256; i++) {
     // reference the specific element that was hovered over
     const hoveredElement = event.target;
     // add class to the hovered element
+       hoveredElement.classList.remove("grid");
        hoveredElement.classList.add("colored");
   });
 }
 
-
-
-
-    
-/* 
-Hoverladığımızda classı ekleyen kodu yaz.
-neler lazım;
-hoverladığımız element
-hoverladığımız elemente fonksiyonla class ekleme
-addeventlistener ve mouseover kullanma.
-
-soru 1: javascriptte array ile oluşturulan html elementlerini nasıl spesifikleştirilir
- */
 
 

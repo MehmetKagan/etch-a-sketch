5dd25176fc35f6375f6907598862baa145b209b6
MehmetKagan <mehmetkagankokoglu@gmail.com>
2023-03-29 23:49:32 +0300

    docs: Create js functions, make reset and black button useful, change margin in left menu
    create functions (1- create a info paragraph for slider value which changes grid values at the same time. 2- create a function which changes div count in container depends on slider. 3- create a function which make changes in the div dimensions depends on div count on container. 4- create some functions for black and reset buttons. ) Add for each and mouseover method for hover effect. Add style classes for black button and left menu margin
    
    Change technique of hover. Solved the problem which cause trouble about changing div count by slider.

--- script.js
+++ script.js
@@ -1,20 +1,93 @@
 const container = document.getElementById("container");
+//-------------------Slider-------------------//
 
-const divs = [];
+const slider = document.getElementById("slider");
+const gridCountPara = document.getElementById("gridCountPara");
+gridCountPara.textContent = `${slider.value}x${slider.value}`;
+let divCount = slider.value * slider.value;
+let sizeOfPixels = 624 / slider.value;
 
-for (let i = 0; i < 256; i++) {
+function values() {
+  gridCountPara.textContent = `${slider.value}x${slider.value}`;
+  divCount = slider.value * slider.value;
+  sizeOfPixels = 624 / slider.value;
+}
+function changeSizes() { 
+const allDivs = container.querySelectorAll("div");
+  allDivs.forEach(div => {
+    div.style.height = `${sizeOfPixels}px`;
+    div.style.width = `${sizeOfPixels}px`;
+  }
+  ) 
+}
+
+function newDivCount() {
+  container.replaceChildren();
+  let divCount = slider.value ** 2;
+  let divs = [];
+  for (let i = 0; i < divCount; i++) {
     divs[i] = document.createElement("div");
     container.appendChild(divs[i]);
     divs[i].classList.add("grid");
- 
-   divs[i].addEventListener("mouseover", (event) => {
-    // reference the specific element that was hovered over
-    const hoveredElement = event.target;
-    // add class to the hovered element
-       hoveredElement.classList.remove("grid");
-       hoveredElement.classList.add("colored");
-  });
+  }
 }
 
+function coloring() {
+  let childDivs = container.querySelectorAll("div");
+  childDivs.forEach(div => {
+  div.addEventListener('mouseover', () => {
+    div.classList.add('colored');
+    div.classList.remove('grid');
+  });
+});
+}
+//-----------------Container Divs-------------------//
+
+ let divs = [];
+  for (let i = 0; i < 256; i++) {
+    divs[i] = document.createElement("div");
+    container.appendChild(divs[i]);
+    divs[i].classList.add("grid");
+  }
+let childDivs = container.querySelectorAll("div");
+  childDivs.forEach(div => {
+  div.addEventListener('mouseover', () => {
+    div.classList.add('colored');
+    div.classList.remove('grid');
+  });
+});
+
+//------------Reset Button-----------//
+const resetButton = document.getElementById("reset");
+const coloredDivs = container.getElementsByClassName("colored");
+const blackDivs = container.getElementsByClassName("black");
+function removeGridClass() {
+  const coloredArray = Array.from(coloredDivs);
+  const blackarray = Array.from(blackDivs);
+  const combinedArray = Array.from(coloredDivs).concat(Array.from(blackDivs));
+  for (const grids of combinedArray)
+  { 
+    grids.classList.remove('black');
+    grids.classList.remove('colored');
+    grids.classList.add('grid');
+}
+};
+resetButton.addEventListener("click", removeGridClass)
+  
+//---------------Black Button-------------------//
 
 
+
+const blackButton = document.getElementById("black");
+function blackIt() { 
+let childDivs = container.querySelectorAll("div");
+  childDivs.forEach(div => {
+    div.addEventListener('mouseover', () => {
+      div.classList.remove('grid');
+      div.classList.remove('colored');
+      div.classList.add('black')
+   
+  });
+});
+}
+blackButton.addEventListener("click", blackIt);
